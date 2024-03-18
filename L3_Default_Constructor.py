d51d0d6de76d72ba54b0bfadf7621f919bc6fe71
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-03-19 04:41:11 +0600

    default constructor method example added

---
+++ L3_Default_Constructor.py
@@ -0,0 +1,13 @@
+"""Class Object Constructor attributes methods()"""
+
+class Student:
+        def __init__(self):
+                print(self)
+                print("A student object is crated")
+
+#--------------------------------------------------------------
+# variable = class_name()
+s1 = Student()
+s2 = Student()
+
+print("s1",s1)
