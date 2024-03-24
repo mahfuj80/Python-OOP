10907ca6a84d68a144a8f05357b36d683aa473a4
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-03-24 21:34:52 +0600

    added another example / alternative of method overloading

--- L10_to_continue/L12_Python_OOP_Method_Overloading.py
+++ L10_to_continue/L12_Python_OOP_Method_Overloading.py
@@ -1,13 +1,23 @@
 class my_calculator:
 
+        def product(self, num1 = None, num2 = None, num3 = None):
+                if num1 != None and num2 != None and num3 != None:
+                        print(num1 * num2 * num3)
+                elif num1 != None and num2 != None:
+                        print(num1 * num2)
+                else:
+                        print(1 *num1)
+
         def product(self, *nums):
                 print(nums)
-                sum = 1
+                pro = 1
                 for x in nums:
-                        sum = sum * x
-                print(sum)
+                        pro = pro * x
+                print(pro)
        
 #================================================================
 
 c1 = my_calculator()
+c1.product(4)
+c1.product(4,5)
 c1.product(4, 2, 2,4,5,6,7 ,7)
