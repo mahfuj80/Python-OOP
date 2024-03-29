801e2f53d863364ca7a385ed23fcf423f92c8b26
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-03-29 22:10:01 +0600

    Added link of different types of method

---
+++ L10_to_continue/L22_Methods_Instance_Class_&_Static_Method.py
@@ -0,0 +1,6 @@
+# https://www.youtube.com/watch?v=X5E5oWQ-vok&list=PLvr0Ht-XkB_3NAwjutgaG0-62d2yjG6qz&index=26
+
+
+'''
+    In the above link there availabe of powerpoint example of different methods. Class Instance and Static method.
+'''
