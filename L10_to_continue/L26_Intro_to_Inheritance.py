9c42f8b8a5a48ebdedf7d7a16695810c7bb5bb72
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-03-30 01:01:41 +0600

    Added Inheritance explaination

---
+++ L10_to_continue/L26_Intro_to_Inheritance.py
@@ -0,0 +1,11 @@
+# https://www.youtube.com/watch?v=iPaPFXh8Oqo&list=PLvr0Ht-XkB_3NAwjutgaG0-62d2yjG6qz&index=30
+
+'''
+What is Inheritance explained avobe in the link
+There are many types of inheritance:
+    1. Single inheritance
+    2. Multiple inheritance
+    3. Multilevel inheritance
+    4. Hybrid inheritance
+    5. Hieraricical inheritance
+'''
