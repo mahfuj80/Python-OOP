50d1378e3950ebd4faf749a3c434e1777df0fab7
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-04-02 02:10:15 +0600

    Added examplel of single inheritance

---
+++ L10_to_continue/L28_Single_Inheritance.py
@@ -0,0 +1,21 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    
+    def eat(self):
+        print(self.name + " is eating")
+
+#================================================================
+
+class Dog(Animal):
+    def bark(self):
+        print(self.name + " is barking")
+
+#================================================================
+
+a1 = Animal("Jack")
+d1 = Dog("Rover")
+
+d1.bark()
+d1.eat()
+a1.eat()
