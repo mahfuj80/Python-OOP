4d15dc93db1a61c75dff8be83d3d398a3fb60bd1
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-04-02 01:54:07 +0600

    Hybrid, Multiple class example added

--- L10_to_continue/L27_Intro_to_Inheritance2.py
+++ L10_to_continue/L27_Intro_to_Inheritance2.py
@@ -15,15 +15,15 @@ m1.method1()
 # Multilevel Inheritance Example
 class ParentClass:
     def method1(self):
-        print("This method1 is in ParentClass")
+        print("This method1 is in ParentClass Multilevel Inheritance")
 
 class ChildClass1(ParentClass):
     def method2(self):
-        print("This method2 is in ChildClass1")
+        print("This method2 is in ChildClass1 Multilevel Inheritance")
 
 class ChildClass2(ChildClass1):
     def method3(self):
-        print("This method3 is in ChildClass2")
+        print("This method3 is in ChildClass2 Multilevel Inheritance")
 
 ch1 = ChildClass2()
 ch1.method1()
@@ -33,3 +33,76 @@ ch1.method3()
 #----------------------------------------------------------------
 
 # Hierarchical Inheritance
+
+class ParentClass:
+    def method1(self):
+        print("This method1 is in ParentClass Hierarchical Inheritance")
+
+class ChildClass1(ParentClass):
+    def method2(self):
+        print("This method2 is in ChildClass1 Hierarchical Inheritance")
+
+class ChildClass2(ChildClass1):
+    def method3(self):
+        print("This method3 is in ChildClass2 Hierarchical Inheritance")
+
+class ChildClass3(ChildClass2):
+    def method4(self):
+        print("This method4 is in ChildClass3")
+
+
+
+ch1 = ChildClass1()
+ch2 = ChildClass2()
+
+ch1.method1()
+ch2.method1()
+#----------------------------------------------------------------
+
+
+# Multiple inheritance
+
+class ParentClass1:
+    def method1(self):
+        print("This method1 is in ParentClass1 Multiple Inheritance")
+
+class ParentClass2:
+    def method2(self):
+        print("This method2 is in ParentClass2 Multiple Inheritance")
+
+class ChildClass(ParentClass1, ParentClass2):
+    def method3(self):
+        print("This method3 is in ChildClass Multiple Inheritance")
+
+ch1 = ChildClass()
+ch1.method1()
+ch1.method2()
+ch2.method3()
+
+#----------------------------------------------------------------
+
+# Hybrid inheritance
+
+class ParentClass:
+    def method1(self):
+        print("This method1 is in ParentClass1 Hybrid Inheritance")
+
+class ChildClass1(ParentClass):
+    def method2(self):
+        print("This method2 is in ChildClass1 Hybrid Inheritance")
+
+class ChildClass2(ChildClass1):
+    def method3(self):
+        print("This method3 is in ChildClass2 Hybrid Inheritance")
+
+class ChildClass3(ChildClass1):
+    def method4(self):
+        print("This method4 is in ChildClass3 Hybrid Inheritance")
+
+ch1 = ChildClass3()
+
+ch1.method1()
+ch1.method2()
+
+#----------------------------------------------------------------
+
